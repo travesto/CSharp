8d1c6bbe4d8b4112cdeb6d2269080475b691b14b
travesto <tdc12c@acu.edu>
2017-02-10 12:00:17 -0600

    Parents

---
+++ Parents/parents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This program prints parent-child pairs.\nEnter parents and children below, use 'quit' to stop.");
+        }
+    }
+}
